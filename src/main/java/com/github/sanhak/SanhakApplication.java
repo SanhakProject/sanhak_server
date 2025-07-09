d69edff363337da2b803ae323f6caaa059a204ae
MinJae <80663462+Dlawoct@users.noreply.github.com>
2025-07-09 18:25:46 +0900

    init: 초기 세팅

---
+++ src/main/java/com/github/sanhak/SanhakApplication.java
@@ -0,0 +1,13 @@
+package com.github.sanhak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanhakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanhakApplication.class, args);
+    }
+
+}
