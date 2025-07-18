4e51e90fbd873e1e1ac022b66ecfefd46c4bd279
MinJae <80663462+Dlawoct@users.noreply.github.com>
2025-07-18 15:50:58 +0900

    feat: Auth Exception 구현

--- src/main/java/com/github/sanhak/auth/exception/AuthException.java
+++ src/main/java/com/github/sanhak/auth/exception/AuthException.java
@@ -1,7 +1,6 @@
 package com.github.sanhak.auth.exception;
 
 import com.github.sanhak.global.exception.CustomException;
-import com.github.sanhak.user.exception.UserExceptionCode;
 
 public class AuthException extends CustomException {
 
