8e433647f14aff7e1b4459f2af44acbf4bf22d97
MinJae <80663462+Dlawoct@users.noreply.github.com>
2025-07-18 15:44:14 +0900

    feat: jwt 토큰 발급을 위한 기능 구현

---
+++ src/main/java/com/github/sanhak/global/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,106 @@
+package com.github.sanhak.global.security.jwt;
+
+import com.github.sanhak.global.property.JwtProperty;
+import com.github.sanhak.global.security.jwt.exception.JwtAuthenticationException;
+import com.github.sanhak.global.security.jwt.exception.JwtAuthenticationExceptionCode;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final JwtProperty jwtProperty;
+    private Key key;
+    private static final String AUTHORITIES_KEY = "auth";
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(jwtProperty.getKey().getBytes());
+    }
+
+    public String createAccessToken(Authentication authentication) {
+        return createToken(authentication, jwtProperty.getAccessTokenValidityInMilliseconds());
+    }
+
+// 리프레쉬 토큰 필요시 사용
+//    public String creatRefreshToken(Authentication authentication) {
+//        return createToken(authentication, jwtProperty.getRefreshTokenValidityInMilliseconds());
+//    }
+
+    private String createToken(Authentication authentication, long expireTime) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = System.currentTimeMillis();
+        Date tokenExpiresIn = new Date(now + expireTime);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .setExpiration(tokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseClaims(token);
+
+        if (claims.get(AUTHORITIES_KEY) == null) {
+            throw new JwtAuthenticationException(JwtAuthenticationExceptionCode.MISSING_AUTHORITY);
+        }
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(BEARER_PREFIX.length());
+        }
+        return null;
+    }
+
+    private Claims parseClaims(String token) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+}
