c691a53764a1bbd0c3bf879630c3cac4f2d14075
MinJae <80663462+Dlawoct@users.noreply.github.com>
2025-07-18 15:50:41 +0900

    feat: Auth 서비스 및 컨트롤러 구현

--- src/main/java/com/github/sanhak/auth/controller/dto/request/LoginRequest.java
+++ src/main/java/com/github/sanhak/auth/controller/dto/request/LoginRequest.java
@@ -2,7 +2,6 @@ package com.github.sanhak.auth.controller.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import static com.github.sanhak.global.regexp.RegExps.PHONE_NUMBER;
